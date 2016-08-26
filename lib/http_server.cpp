d22d93fa058242a5c31e820f53af5b7ae2676f5e
ao-streetart <aostreetart9@gmail.com>
2016-08-26 18:24:37 -0400

    HTTP Server fixes

--- lib/http_server.cpp
+++ lib/http_server.cpp
@@ -16,7 +16,7 @@ bool HttpServer::bind_callback(std::string url, int port, HttpCallback func)
   struct evhttp *httpd;
   httpd = evhttp_new(base);
   if (httpd == NULL) return -1;
-  if (evhttp_bind_socket(httpd, url, port) != 0) return -1;
+  if (evhttp_bind_socket(httpd, url.c_str(), port) != 0) return -1;
   evhttp_set_gencb(httpd, func, NULL);
 }
 
