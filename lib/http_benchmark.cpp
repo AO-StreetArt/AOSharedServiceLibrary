06260883686284fdcfe04e493115ae8fc08181c5
ao-streetart <aostreetart9@gmail.com>
2016-10-01 23:57:19 -0400

    HTTP Admin Fixes

--- lib/http_benchmark.cpp
+++ lib/http_benchmark.cpp
@@ -12,35 +12,14 @@
 #include "include/factory_logging.h"
 #include "include/factory_http_client.h"
 
-//----------------------------HTTP Callbacks----------------------------------//
-
-//A String to store response data
-std::string write_data;
-
-//This is the callback that gets called when we recieve the response to the
-//Get Curl Request
-size_t write_Callback(char * buf, size_t size, size_t nmemb, void* up)
-{
-
-  logging->debug("Callback Triggered");
-
-//Put the response into a string
-for (int c = 0; c<size*nmemb; c++)
-{
-	write_data.push_back(buf[c]);
-}
-
-return size*nmemb;
-}
-
 //----------------------------------------------------------------------------//
 //--------------------------------Globals-------------------------------------//
 //----------------------------------------------------------------------------//
 
-char *POSTURL;
-char *PUTURL;
-char *GETURL;
-char *DELETEURL;
+std::string post = "http://httpbin.org/post";
+std::string put = "http://httpbin.org/put";
+std::string get = "http://httpbin.org/get";
+std::string del = "http://httpbin.org/delete";
 
 HttpInterface *http;
 
@@ -51,20 +30,12 @@ HttpInterface *http;
 BENCHMARK(HTTP, Get, 10, 100)
 {
 
-  //Clear the return string
-  write_data.clear();
 
   //Send the request
-  bool success = http->get(GETURL, 5);
-  if (!success)
+  std::string ret_val = http->get(get, 5);
+  if (ret_val.empty())
   {
-    //We now have the full response
     logging->error("Get Request Failed");
-  }
-  else
-  {
-    logging->debug("Retrieved:");
-    logging->debug(write_data);
   }
 
 }
@@ -72,20 +43,13 @@ BENCHMARK(HTTP, Get, 10, 100)
 BENCHMARK(HTTP, Put, 10, 100)
 {
 
-  //Clear the return string
-  write_data.clear();
 
   //Send the request
-  bool success = http->put(PUTURL, "123", 5);
+  bool success = http->put(put, "123", 5);
   if (!success)
   {
     //We now have the full response
     logging->error("Put Request Failed");
-  }
-  else
-  {
-    logging->debug("Retrieved:");
-    logging->debug(write_data);
   }
 
 }
@@ -93,20 +57,12 @@ BENCHMARK(HTTP, Put, 10, 100)
 BENCHMARK(HTTP, Post, 10, 100)
 {
 
-  //Clear the return string
-  write_data.clear();
-
   //Send the request
-  bool success = http->post(POSTURL, "CLYMAN", 5);
+  bool success = http->post(post, "CLYMAN", 5);
   if (!success)
   {
     //We now have the full response
-    logging->error("Get Request Failed");
-  }
-  else
-  {
-    logging->debug("Retrieved:");
-    logging->debug(write_data);
+    logging->error("Post Request Failed");
   }
 
 }
@@ -114,20 +70,12 @@ BENCHMARK(HTTP, Post, 10, 100)
 BENCHMARK(HTTP, Delete, 10, 100)
 {
 
-  //Clear the return string
-  write_data.clear();
-
   //Send the request
-  bool success = http->del(DELETEURL, 5);
+  bool success = http->del(del, 5);
   if (!success)
   {
     //We now have the full response
-    logging->error("Get Request Failed");
-  }
-  else
-  {
-    logging->debug("Retrieved:");
-    logging->debug(write_data);
+    logging->error("Delete Request Failed");
   }
 
 }
@@ -138,22 +86,6 @@ BENCHMARK(HTTP, Delete, 10, 100)
 
 int main()
 {
-
-  //Variables to store URL's
-  std::string post = "http://httpbin.org/post";
-  std::string put = "http://httpbin.org/put";
-  std::string get = "http://httpbin.org/get";
-  std::string del = "http://httpbin.org/delete";
-
-  POSTURL = new char[post.length() + 1];
-  PUTURL = new char[put.length() + 1];
-  GETURL = new char[get.length() + 1];
-  DELETEURL = new char[del.length() + 1];
-
-  strcpy(POSTURL, post.c_str());
-  strcpy(PUTURL, put.c_str());
-  strcpy(GETURL, get.c_str());
-  strcpy(DELETEURL, del.c_str());
 
   HttpClientFactory http_client_factory;
   LoggingComponentFactory logging_factory;
@@ -168,11 +100,6 @@ logging->info("Internal Logging Intialized");
 //Set up HTTP Client
 http = http_client_factory.get_http_interface();
 logging->info("HTTP Outbound Interface Created");
-
-//We set up the structure to store the return data
-write_data.clear();
-
-http->bind_get_callback(write_Callback);
 
 //------------------------------Run Tests-------------------------------------//
 //----------------------------------------------------------------------------//
--- lib/http_test.cpp
+++ lib/http_test.cpp
@@ -1,6 +1,4 @@
 #include "include/http_admin.h"
-#include "include/factory/logging_interface.h"
-#include "include/logging.h"
 
 #include <string>
 #include <string.h>
@@ -8,27 +6,6 @@
 #include <iostream>
 #include <fstream>
 #include <assert.h>
-
-//----------------------------HTTP Callbacks----------------------------------//
-
-//A String to store response data
-std::string writedata;
-
-//This is the callback that gets called when we recieve the response to the
-//Get Curl Request
-size_t writeCallback(char * buf, size_t size, size_t nmemb, void* up)
-{
-
-  logging->debug("Callback Triggered");
-
-//Put the response into a string
-for (int c = 0; c<size*nmemb; c++)
-{
-	writedata.push_back(buf[c]);
-}
-
-return size*nmemb;
-}
 
 //-----------------------------MAIN METHOD------------------------------------//
 
@@ -42,59 +19,28 @@ int main()
     std::string get = "http://httpbin.org/get";
     std::string del = "http://httpbin.org/delete";
 
-    char *POSTURL = new char[post.length() + 1];
-    strcpy(POSTURL, post.c_str());
-
-    char *PUTURL = new char[put.length() + 1];
-    strcpy(PUTURL, put.c_str());
-
-    char *GETURL = new char[get.length() + 1];
-    strcpy(GETURL, get.c_str());
-
-    char *DELETEURL = new char[del.length() + 1];
-    strcpy(DELETEURL, del.c_str());
-
-    //Logging
-
-    std::string initFileName = "test/log4cpp_test.properties";
-    logging = new Logger(initFileName);
-
     //----------------------------MAIN TEST-----------------------------------//
 
     HttpAdmin ha;
 
-    logging->debug("Get URL");
-    logging->debug(GETURL);
-    logging->debug("Put URL");
-    logging->debug(PUTURL);
-    logging->debug("Post URL");
-    logging->debug(POSTURL);
-    logging->debug("Delete URL");
-    logging->debug(DELETEURL);
-
     //-------------------------------GET--------------------------------------//
 
-    //We set up the structure to store the return data
-    writedata.clear();
-
-    ha.bind_get_callback(writeCallback);
-
     //Send the request
-    bool success = ha.get(GETURL, 5);
-    if (!success)
+    std::string ret_val = ha.get(get, 5);
+    if ( ret_val.empty() )
     {
       //We now have the full response
       assert(false);
     }
     else
     {
-      logging->debug("Retrieved:");
-      logging->debug(writedata);
+      std::cout << ("Retrieved:") << std::endl;
+      std::cout << (ret_val) << std::endl;
     }
 
     //-------------------------------PUT--------------------------------------//
 
-    success = ha.put(PUTURL, "123", 5);
+    success = ha.put(put, "123", 5);
     if (!success)
     {
       //We now have the full response
@@ -103,7 +49,7 @@ int main()
 
     //-------------------------------POST-------------------------------------//
 
-    success = ha.post(POSTURL, "CLYMAN", 5);
+    success = ha.post(post, "CLYMAN", 5);
     if (!success)
     {
       //We now have the full response
@@ -112,14 +58,14 @@ int main()
 
     //------------------------------DELETE------------------------------------//
 
-    success = ha.del(DELETEURL, 5);
+    success = ha.del(del, 5);
     if (!success)
     {
       //We now have the full response
       assert(false);
     }
 
-    logging->debug("Tests completed");
+    std::cout << ("Tests completed") << std::endl;
 
     ha.shutdown();
   delete logging;
