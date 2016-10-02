22ec5156518bb6f9bee5a3c427f5c36c585b4442
ao-streetart <aostreetart9@gmail.com>
2016-10-01 23:28:39 -0400

    HTTP Admin Wrap Callback

--- lib/consul_admin.cpp
+++ lib/consul_admin.cpp
@@ -1,24 +1,5 @@
 #include "include/consul_admin.h"
 
-//Global writedata instantiation to store HTTP Callbacks
-std::string writedata;
-
-//----------------------------HTTP Callbacks----------------------------------//
-
-//This is the callback that gets called when we recieve the response to the
-//Get Curl Request
-size_t writeCallback(char * buf, size_t size, size_t nmemb, void* up)
-{
-
-//Put the response into a string
-for (size_t c = 0; c<size*nmemb; c++)
-{
-	writedata.push_back(buf[c]);
-}
-
-return size*nmemb;
-}
-
 //------------------------Consul Administrator--------------------------------//
 
 std::string ConsulAdmin::base64_decode(std::string const& encoded_string) {
@@ -79,22 +60,15 @@ std::string ConsulAdmin::build_url(std::string request_url_segment)
 //Post a query to consul
 std::string ConsulAdmin::query(std::string query_url)
 {
-  //Clear the string that will hold the response data.
-  writedata.clear();
   //Get the URL
   std::string url_string = build_url(query_url);
-  const char * url_cstr = url_string.c_str();
-  char *url = new char[url_string.length() + 1];
-  strcpy(url, url_cstr);
-
-  ha->bind_get_callback(writeCallback);
 
   //Send the HTTP Request
-  bool success = ha->get(url, timeout);
+  std::string get_resp = ha->get(url_string, timeout);
   delete url;
-  if (success)
+  if ( !get_resp.empty() )
   {
-    return writedata;
+    return get_resp;
   }
   else
   {
@@ -108,18 +82,12 @@ bool ConsulAdmin::register_service(ServiceInterface& s)
 {
   //Get the URL
   std::string url_string = build_url("/v1/agent/service/register");
-  const char * url_cstr = url_string.c_str();
-  char *url = new char[url_string.length() + 1];
-  strcpy(url, url_cstr);
 
   //Get the message body
   std::string body_str = s.to_json();
-  const char * body_cstr = body_str.c_str();
-  char *body = new char[body_str.length() + 1];
-  strcpy(body, body_cstr);
 
   //Send the HTTP Request
-  bool success = ha->put(url, body, timeout);
+  bool success = ha->put(url_string, body_str, timeout);
   delete body;
   delete url;
   return success;
@@ -131,16 +99,10 @@ bool ConsulAdmin::deregister_service(ServiceInterface& s)
   std::string url_string = "/v1/agent/service/deregister/";
   url_string = url_string.append(s.get_id());
   url_string = build_url(url_string);
-  const char * url_cstr = url_string.c_str();
-  char *url = new char[url_string.length() + 1];
-  strcpy(url, url_cstr);
 
   //Send the HTTP Request
   std::string empty_str = "";
-  const char * empty_cstr = empty_str.c_str();
-  char *empty_arr = new char[empty_str.length() + 1];
-  strcpy(empty_arr, empty_cstr);
-  bool success = ha->put(url, empty_arr, timeout);
+  bool success = ha->put(url_string, empty_str, timeout);
   delete empty_arr;
   delete url;
   return success;
@@ -176,17 +138,9 @@ bool ConsulAdmin::set_config_value(std::string key, std::string val)
   //Get the URL
   std::string url_string = build_url("/v1/kv/");
   url_string = url_string.append(key);
-  const char * url_cstr = url_string.c_str();
-  char *url = new char[url_string.length() + 1];
-  strcpy(url, url_cstr);
-
-  //Get the message body
-  const char * body_cstr = val.c_str();
-  char *body = new char[val.length() + 1];
-  strcpy(body, body_cstr);
 
   //Send the HTTP Request
-  bool success = ha->put(url, body, timeout);
+  bool success = ha->put(url_string, val, timeout);
   delete body;
   delete url;
   return success;
@@ -204,12 +158,8 @@ bool ConsulAdmin::del_config_value(std::string key)
   //Get the URL
   std::string url_string = build_url("/v1/kv/");
   url_string = url_string.append(key);
-  const char * url_cstr = url_string.c_str();
-  char *url = new char[url_string.length() + 1];
-  strcpy(url, url_cstr);
-
   //Send the HTTP Request
-  bool success = ha->del(url, timeout);
+  bool success = ha->del(url_string, timeout);
   delete url;
   return success;
 }
