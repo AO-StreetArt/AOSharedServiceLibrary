644d2629315c22bee6a8d56dcd77b619b8833718
ao-streetart <aostreetart9@gmail.com>
2016-10-05 00:40:09 -0400

    Test Scripts fixes

--- lib/uuid_test.cpp
+++ lib/uuid_test.cpp
@@ -1,6 +1,8 @@
 #include "include/uuid_admin.h"
 #include <iostream>
 #include <assert.h>
+#include <string>
+#include <string.h>
 
 int main()
 {
@@ -8,8 +10,16 @@ int main()
   uuidAdmin uuid;
 
   //Generate a UUID
-  std::string uuid_str = uuid.generate();
-  assert ( !uuid_str.empty() );
-  std::cout << uuid_str << std::endl;
+  std::string id_str;
+  try {
+    id_str = ua->generate();
+  }
+  catch (UuidSecurityException& ue) {
+    //Continue and print the security error
+    uuid_logging->error("UUID Security Exception");
+    uuid_logging->error(ue.what());
+  }
+  assert ( !id_str.empty() );
+  std::cout << id_str << std::endl;
 
 }
