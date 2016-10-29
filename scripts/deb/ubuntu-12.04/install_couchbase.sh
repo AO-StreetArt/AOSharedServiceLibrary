f8fc00f1177d6b43090c1397c887075d1e634891
Alex Barry <abarry@localhost.localdomain>
2016-10-29 19:41:18 -0400

    Couchbase Install Scripts Fixed

--- scripts/deb/ubuntu-12.04/install_couchbase.sh
+++ scripts/deb/ubuntu-12.04/install_couchbase.sh
@@ -9,7 +9,7 @@ cd /opt/couchbase
 ./bin/install/reloc.sh `pwd`
 ./bin/couchbase-server -- -noinput -detached
 sleep 20
-./bin/couchbase-cli cluster-init -c 127.0.0.1:8091  --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8091 --cluster-init-ramsize=1024
+./bin/couchbase-cli cluster-init -c 127.0.0.1:8091  --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8091 --cluster-init-ramsize=2048
 ./bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=default --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=408  --bucket-replica=1 -u Administrator -p password
 ./bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=test --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=408  --bucket-replica=1 -u Administrator -p password
 ./bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=test2 --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=408  --bucket-replica=1 -u Administrator -p password
