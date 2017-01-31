d0414640d36416f93b62a430cba5c45bdfc13cb3
ao-streetart <aostreetart9@gmail.com>
2017-01-30 23:01:33 -0500

    Travis CI Fix

--- aossl/interpreter/properties_reader/include/factory_props.h
+++ aossl/interpreter/properties_reader/include/factory_props.h
@@ -27,8 +27,9 @@ THE SOFTWARE.
 
 #include <string>
 
-#include "aossl/interpreter/properties_reader/include/properties_reader_interface.h"
 #include "aossl/interpreter/properties_reader/include/properties_reader.h"
+
+class PropertiesReaderInterface;
 
 //! The Property File Reader Service Component Factory
 
