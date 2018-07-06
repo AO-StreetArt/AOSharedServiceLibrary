3c3b46c047fbbb5536f26a0cbe637b860914b07c
Alex Barry <aostreetart9@gmail.com>
2018-07-06 01:37:44 -0400

    update profile test setup based on new key name routine

--- scripts/profile_test_setup.sh
+++ scripts/profile_test_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl --request PUT -d five http://localhost:8500/v1/kv/test/prof/KONE
-curl --request PUT -d six http://localhost:8500/v1/kv/test/prof/KTWO
-export FIRST=seven
-export SECOND=eight
+curl --request PUT -d five http://localhost:8500/v1/kv/test/prof/TEST_PROF_KONE
+curl --request PUT -d six http://localhost:8500/v1/kv/test/prof/TEST_PROF_KTWO
+export TEST_PROF_FIRST=seven
+export TEST_PROF_SECOND=eight
