1e94f59bc1c94df47d4c9b80c097fc89b26918c6
Alex Barry <aostreetart9@gmail.com>
2018-07-07 18:28:52 -0400

    update vault test setup with new config key start

--- scripts/profile_test_setup.sh
+++ scripts/profile_test_setup.sh
@@ -4,3 +4,6 @@ curl --request PUT -d five http://localhost:8500/v1/kv/test/prof/TEST_PROF_KONE
 curl --request PUT -d six http://localhost:8500/v1/kv/test/prof/TEST_PROF_KTWO
 export TEST_PROF_FIRST=seven
 export TEST_PROF_SECOND=eight
+
+./vault kv put secret/TEST_PROF_TESTKEY TEST_PROF_TESTKEY=testValue
+./vault kv put secret/TEST_PROF_TESTKEY TEST_PROF_TESTKEY=testValue
