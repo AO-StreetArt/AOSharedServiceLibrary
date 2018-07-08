ae5d487cd32534bfb2bd6596558d7403cd37abc7
Alex Barry <aostreetart9@gmail.com>
2018-07-07 23:15:02 -0400

    set vault addr again in profile test setup script

--- scripts/profile_test_setup.sh
+++ scripts/profile_test_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export VAULT_ADDR='http://127.0.0.1:8200'
+
 curl --request PUT -d five http://localhost:8500/v1/kv/test/prof/TEST_PROF_KONE
 curl --request PUT -d six http://localhost:8500/v1/kv/test/prof/TEST_PROF_KTWO
 export TEST_PROF_FIRST=seven
